5c0fd6ab3700a75f8c6089864cd32e9fc49f32cb
Taraqur <tqrahman8@gmail.com>
2022-07-23 22:13:02 -0400

    plotted image

--- homeApp/forms.py
+++ homeApp/forms.py
@@ -2,11 +2,11 @@ from django import forms
 
 from .models import Image, StyleImage
 
-class ImageForm(forms.ModelForm):
+class ImageForm(forms.ModelForm): 
     class Meta:
         model = Image
         fields = {'first_image', 'second_image'}
-
+    
 class ImageFormStyleTransfer(forms.ModelForm):
     class Meta:
         model = StyleImage
